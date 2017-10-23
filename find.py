bfe694946a7f30b930b1e731f29cc42bb2d4d6e0
msjahid <nirjonjahid@gmail.com>
2017-10-24 03:59:25 +0600

    find function

--- find.py
+++ find.py
@@ -1,3 +1,14 @@
+sentence = "bob saw a big blue boat"
+b = 0
+x = sentence.count('b')
+for i in range (x):
+    a = 0 + b
+    b = sentence.find('b',a)
+    print(b)
+    b = b + 1
+
+
+'''
 sentence = "how can a clam cram in a clean cream can?"
 b = 0
 x = sentence.count('c')
@@ -8,5 +19,8 @@ for i in range (x):
         a = 1 + b
         b = sentence.find('c',a)
         print(b)
+
+'''
+
         
     
